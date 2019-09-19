79d4f960fa39d9c91555a9b9e67543ce1fc106ba
Edson Servin <edson@ciencias.unam.mx>
2019-09-19 10:59:31 -0500

    coding master adding thymeleaf for the html page and running the app ./gradle bootRun

--- src/main/groovy/camel/training/controllers/TestController.groovy
+++ src/main/groovy/camel/training/controllers/TestController.groovy
@@ -3,16 +3,13 @@ package camel.training.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-@RequestMapping("/welcome")
 class TestController {
 
-    @GetMapping
-    @ResponseBody
-    def getPeriods(){
-        "Hello get"
-    }
+	@GetMapping("/welcome")
+	def test(){
+		"welcome"
+	}
     
 }
