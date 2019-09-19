82f9c939e2e2540280e7f98a6192316845349fe2
Edson Servin <edson@ciencias.unam.mx>
2019-09-18 20:49:27 -0500

    coding master adding the default configuration for gradle, spring boot and groovy

--- src/main/groovy/camel/training/App.groovy
+++ src/main/groovy/camel/training/App.groovy
@@ -3,12 +3,16 @@
  */
 package camel.training
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
 class App {
     String getGreeting() {
         return 'Hello world.'
     }
 
     static void main(String[] args) {
-        println new App().greeting
+			SpringApplication.run(App, args)	
     }
 }
