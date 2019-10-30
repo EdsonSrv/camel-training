d28fb6d7ab9158e393944c1b4e0ce13ac81d340a
Edson Srvn <edson@makingdevs.com>
2019-10-30 09:53:31 -0600

    coding master Adding cases in route test

--- bin/main/com/example/camel/routes/RouteTest.groovy
+++ bin/main/com/example/camel/routes/RouteTest.groovy
@@ -10,8 +10,14 @@ class RouteTest extends RouteBuilder {
   @Override
   void configure() throws Exception {
     from("file://src/test/resources/filesIn?")
-      .log("...Moving \${headers['CamelFileNameOnly']} file...")
-      .to("file://src/test/resources/filesOut")
-      .log("...File \${headers['CamelFileNameOnly']} moved...")
+      .choice()
+        .when(header("CamelFileNameOnly").endsWith(".01.out"))
+          .log("...Moving \${headers['CamelFileNameOnly']} file...")
+          .to("file://src/test/resources/filesResponses")
+          .log("...File \${headers['CamelFileNameOnly']} moved to RESPONSES...")
+        .when(header("CamelFileNameOnly").endsWith(".out"))
+          .log("...Moving \${headers['CamelFileNameOnly']} file...")
+          .to("file://src/test/resources/filesAcknowledgment")
+          .log("...File \${headers['CamelFileNameOnly']} moved to ACKNOWLEDGMENT...")
   }
 }
