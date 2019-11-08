ed36ba119e125fcca5e195162415b7399dd4d73c
Edson Srvn <edson@makingdevs.com>
2019-11-08 10:22:53 -0600

    coding master Adding set header example

--- src/main/groovy/com/example/camel/routes/RouteTest.groovy
+++ src/main/groovy/com/example/camel/routes/RouteTest.groovy
@@ -3,6 +3,8 @@ package com.example.camel.routes
 import org.springframework.stereotype.Component
 import org.apache.camel.impl.*
 import org.apache.camel.builder.*
+import org.apache.camel.Exchange
+import org.apache.camel.Message
 
 @Component
 class RouteTest extends RouteBuilder {
@@ -19,5 +21,19 @@ class RouteTest extends RouteBuilder {
           .log("...Moving \${headers['CamelFileNameOnly']} file...")
           .to("file://src/test/resources/filesAcknowledgment")
           .log("...File \${headers['CamelFileNameOnly']} moved to ACKNOWLEDGMENT...")
+
+		from("file://src/test/resources/filesTest?noop=true")
+      .log("**********************INIT**************************")
+      .to("log:DEBUG?showBody=false&showHeaders=true")
+      .process { Exchange exchange ->
+        exchange.getIn().setHeader("backupPath", "path................path")
+        // String outputFile =  (headers.CamelFileNameOnly.matches(/.*\.[0-9]{2}.out$/))?
+        //   "${pathResponseOutTemp}/${headers.CamelFileNameOnly}" : "${pathAcknowledgmentOutTemp}/${headers.CamelFileNameOnly}"
+      }
+      .log("---------------------------------------------------")
+      .log("\${headers['backupPath']}")
+      .log("---------------------------------------------------")
+      .to("log:DEBUG?showBody=false&showHeaders=true")
+      .log("**********************END**************************")
   }
 }
